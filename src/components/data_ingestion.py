923653d23458752c73df2002c11f29984629bf05
alibahja <ali.bahja@lau.edu>
2026-08-30 22:12:22 +0300

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 #any data given should pass through this class
 @dataclass #able to directly define class variables
 class DataIngestionConfig:
@@ -52,4 +55,5 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
-    
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
